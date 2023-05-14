1763b6e6c09a93e907c1106b1b0574dadedbf0f8
DanPlayz <damuffin36@gmail.com>
2023-05-14 03:37:42 -0700

    Better method for hiding IP on speedtest.net

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-const xpathExpression = "/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[4]/div/div[2]/div/div[1]/div/div[3]";
+function waitForElm(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
 
-const elementToRemove = document.evaluate(xpathExpression, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      const observer = new MutationObserver(mutations => {
+        if (document.querySelector(selector)) {
+          resolve(document.querySelector(selector));
+          observer.disconnect();
+        }
+      });
 
-if (elementToRemove)
-{
-      elementToRemove.innerHTML = '';
-}
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+    });
+  }
+  await waitForElm(".ispComponent .result-data");
+  let ipAddress = document.querySelector(".ispComponent .result-data").innerText;
 
-const xpathExpression2 = "/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[4]/div/div/div[1]/div[3]/div[3]";
+  document.addEventListener('click', (e) => {
+    if (e.srcElement.id == "ip-reveal-btn") {
+      e.srcElement.parentNode.className += " ip-revealed";
+      e.srcElement.parentNode.innerHTML = ipAddress;
+    }
+  });
 
-const elementToRemove2 = document.evaluate(xpathExpression, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  setInterval(() => {
+    const ispComponentIP = document.querySelector(".ispComponent .result-data");
+    if (ispComponentIP && !ispComponentIP.className.includes("ip-revealed") && ispComponentIP.innerText.includes(ipAddress)) {
+      ispComponentIP.innerHTML = '<a id="ip-reveal-btn">Click to reveal</a>';
+    }
 
-if (elementToRemove2)
-{
-      elementToRemove2.innerHTML = '';
-}
+    const resultDataIP = document.querySelector(".js-data-ip");
+    if (resultDataIP && !resultDataIP.className.includes("ip-revealed") && resultDataIP.innerText.includes(ipAddress)) {
+      resultDataIP.innerHTML = '<a id="ip-reveal-btn">Click to reveal</a>';
+    }
+  }, 10);
